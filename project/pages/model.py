dd6634f8610ce8e461f02a0841c5012ef3888daa
김효원 <kanna28051@gmail.com>
2025-06-20 19:53:28 +0900

    Delete project/pages/model.py

--- project/pages/model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def run_monte_carlo(base_damage, policy_effectiveness=0.1, simulations=1000):
-    results = []
-    for _ in range(simulations):
-        random_factor = np.random.normal(loc=1 - policy_effectiveness, scale=0.05)
-        result = max(base_damage * random_factor, 0)
-        results.append(result)
-    return results
