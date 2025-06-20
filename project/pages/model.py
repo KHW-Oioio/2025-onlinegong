54c096a18319a156fcfecdb76b42f2fe4b71258c
김효원 <kanna28051@gmail.com>
2025-06-20 12:49:09 +0900

    Update model.py

--- project/pages/model.py
+++ project/pages/model.py
@@ -1,19 +1,8 @@
-import numpy as np, networkx as nx
+import numpy as np
 
-REGION_EDGES = [("Seoul","Busan"),("Seoul","Daegu"),("Busan","Daegu")]
-
-def build_graph(nodes):
-    G = nx.Graph()
-    for n in nodes: G.add_node(n)
-    for u,v in REGION_EDGES:
-        if u in nodes and v in nodes:
-            G.add_edge(u,v)
-    return G
-
-def run_mc(base, mean, std, precip, wind, policy=0.0, n=1000):
+def run_policy_simulation(base_damage, temp_increase, policy_effectiveness, iterations=1000):
     np.random.seed(42)
-    t = np.random.normal(mean,std,n)
-    p = np.random.choice(precip,n)
-    w = np.random.choice(wind,n)
-    dmg = base*(1+0.25*t)*(1+0.1*p/100)*(1+0.05*w/10)
-    return dmg*(1-policy)
+    random_temp = np.random.normal(temp_increase, 0.5, iterations)
+    damages = base_damage * (1 + 0.2 * random_temp)
+    damages *= (1 - policy_effectiveness)
+    return damages
