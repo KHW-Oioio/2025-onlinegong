9091c538c79bf1764d8810d744f674084021b226
김효원 <kanna28051@gmail.com>
2025-06-20 12:03:07 +0900

    Update model.py

--- project/pages/model.py
+++ project/pages/model.py
@@ -1,34 +1,19 @@
 import numpy as np, networkx as nx
 
-REGION_EDGES = [
-    ("RegionA", "RegionB"),
-    ("RegionA", "RegionC"),
-    ("RegionB", "RegionC"),
-]
+REGION_EDGES = [("Seoul","Busan"),("Seoul","Daegu"),("Busan","Daegu")]
 
-def build_graph(regions):
+def build_graph(nodes):
     G = nx.Graph()
-    for r in regions:
-        G.add_node(r)
-    for u, v in REGION_EDGES:
-        if u in regions and v in regions:
-            G.add_edge(u, v, weight=1)
+    for n in nodes: G.add_node(n)
+    for u,v in REGION_EDGES:
+        if u in nodes and v in nodes:
+            G.add_edge(u,v)
     return G
 
-
-def run_monte_carlo(base_damage, mean_temp, std_temp,
-                    precip, wind, policy_reduction=0.0,
-                    iterations=1000):
+def run_mc(base, mean, std, precip, wind, policy=0.0, n=1000):
     np.random.seed(42)
-    temp   = np.random.normal(mean_temp, std_temp, iterations)
-    precip = np.random.choice(precip, iterations)
-    wind   = np.random.choice(wind, iterations)
-
-    damages = (
-        base_damage *
-        (1 + 0.25*temp) *
-        (1 + 0.1*precip/100) *
-        (1 + 0.05*wind/10)
-    )
-    damages *= (1 - policy_reduction)
-    return damages
+    t = np.random.normal(mean,std,n)
+    p = np.random.choice(precip,n)
+    w = np.random.choice(wind,n)
+    dmg = base*(1+0.25*t)*(1+0.1*p/100)*(1+0.05*w/10)
+    return dmg*(1-policy)
